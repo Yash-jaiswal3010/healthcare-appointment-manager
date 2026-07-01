8b4a8bf0cb2c57bdc1388f8f5f483cb210fdc1af
Yash Jaiswal <yashjaiswal8899@gmail.com>
2026-07-01 12:46:47 +0530

    Implemented JWT authentication APIs

---
+++ accounts/serializers.py
@@ -0,0 +1,64 @@
+from rest_framework import serializers
+from .models import User
+from django.contrib.auth import authenticate
+from rest_framework_simplejwt.tokens import RefreshToken
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+# meta tells about the extra essential info like if i don't want to give permission for becoming admin after registeration then i will not mention kit on meta, element inside meta will be considered onlyy
+    class Meta:
+        model = User
+        fields = [
+            "full_name",
+            "email",
+            "password",
+        ]
+# extra_kwargs-->the API response should never include a password. DRF has a solution for that, and it's called extra_kwargs.
+        extra_kwargs = {
+            "password": {
+                "write_only": True
+            }
+        }
+# validated_data-> it takes api input from frontend
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
+
+# login serializer
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, attrs):
+        user = authenticate(
+            email=attrs["email"],
+            password=attrs["password"]
+        )
+
+        if not user:
+            raise serializers.ValidationError(
+                "Invalid email or password"
+            )
+
+        refresh = RefreshToken.for_user(user)
+
+        return {
+            "refresh": str(refresh),
+            "access": str(refresh.access_token),
+            "user": {
+                "id": user.id,
+                "full_name": user.full_name,
+                "email": user.email,
+                "role": user.role,
+            },
+        }
+    
+class ProfileSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = [
+            "id",
+            "full_name",
+            "email",
+            "role",
+        ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import generics,status
+from .models import User
+from .serializers import RegisterSerializer,LoginSerializer,ProfileSerializer
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+
+class LoginView(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        return Response(
+            serializer.validated_data,
+            status=status.HTTP_200_OK
+        )
+
+
+class ProfileView(generics.RetrieveAPIView):
+    serializer_class = ProfileSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
